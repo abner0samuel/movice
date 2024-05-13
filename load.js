b0e8a53ff392c22dafe69fb836bdec01b9fc6560
abner0samuel <153988389+abner0samuel@users.noreply.github.com>
2024-05-13 14:24:22 +0100

    Add files via upload
    add some styling and functions

---
+++ load.js
@@ -0,0 +1,10 @@
+const btn=document.querySelector("#menu-button");
+const rootElement =document.documentElement
+btn.computedStyleMap
+
+btn.addEventListener("click",()=>{
+    rootElement.toggleAttribute('inner')
+
+
+
+})
